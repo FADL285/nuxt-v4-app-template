963c5228b2d7f8cf52ff8f49595ecc479e6b47c2
Mohamed Fadl <mohamed.fadl.farag@gmail.com>
2025-01-04 01:44:53 +0200

    Refactor app layout and add home page with navigation components

---
+++ app/pages/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const items = [
+  {
+    label: "Home",
+    icon: "i-lucide-house"
+  },
+  {
+    label: "Components",
+    icon: "i-lucide-box",
+    to: "/components"
+  },
+  {
+    label: "Breadcrumb",
+    icon: "i-lucide-link",
+    to: "/components/breadcrumb"
+  }
+]
+</script>
+
+<template>
+  <div>
+    <UBreadcrumb :items="items" />
+
+    <h1>Home page</h1>
+    <p>Content of the home page</p>
+    <UButton color="neutral">Button</UButton>
+  </div>
+</template>
+
+<style scoped></style>
